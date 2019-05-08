6a7e0fa97ec4ee0ce6b55b8b7a50feac4618bbf8
sheridanliew <league@imac-2.attlocal.net>
2019-05-07 18:33:01 -0700

    League Invaders (Runner, Game Background)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame gameFrame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel gp;
 
+	public LeagueInvaders() {
+		gameFrame = new JFrame();
+		gp = new GamePanel();
+	}
+
+	void setup() {
+		gameFrame.add(gp);
+		gameFrame.setSize(WIDTH, HEIGHT);
+		gameFrame.setVisible(true);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		LeagueInvaders li = new LeagueInvaders();
+		li.setup();
+	}
 }
